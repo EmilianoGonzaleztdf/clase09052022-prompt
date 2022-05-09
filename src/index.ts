736a18c8478e517d74b99d9d9fbfaba22fee1763
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-09 20:25:10 -0300

    test de visto en clases promediador de notas via console.log

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 let nota, suma, promedio, contador: number;
 contador = 1;
+suma = 0;
+while (contador <= 10) {
+  nota = Number(prompt("ingrese una nota: "));
+  suma = suma + nota;
+  contador = contador + 1;
+}
+promedio = suma / 10;
+console.log("el promedio de las notas es: " + promedio);
